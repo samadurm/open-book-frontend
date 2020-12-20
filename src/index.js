f685cf094dc2bdcd16beb67ab2826daa7df30d79
miguelgas <gasparmm@oregonstate.edu>
2020-12-19 19:03:13 -0800

    added basic sign in/up, working on styling now

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 import { BrowserRouter } from 'react-router-dom';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
 
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { Auth0Provider } from "@auth0/auth0-react";
+import config from "./auth_config.json";
+import history from "./utils/history";
+
+const onRedirectCallback = (appState) => {
+    history.push(
+        appState && appState.returnTo
+            ? appState.returnTo
+            : window.location.pathname
+    );
+};
 
 ReactDOM.render(
     <BrowserRouter>
-        <App />
+        <Auth0Provider
+            domain={config.domain}
+            clientId={config.clientId}
+            audience={config.audience}
+            redirectUri={window.location.origin}
+            onRedirectCallback={onRedirectCallback}
+        >
+            <App />
+        </Auth0Provider>
+
     </BrowserRouter>,
     document.getElementById('root')
 );
