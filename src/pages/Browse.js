7279a1c6dd346f9f1b1399aa81ffd0a31de1c929
miguelgas <gasparmm@oregonstate.edu>
2020-12-20 14:54:39 -0800

    implemented live search for search by category

--- src/pages/Browse.js
+++ src/pages/Browse.js
@@ -2,16 +2,16 @@ import CourseCard from '../components/CourseCard';
 import Header from "../components/Header";
 import CardSection from "../components/CardSection";
 import "../css/Browse.css";
+import "../css/Navbar.css";
 /** @jsxRuntime classic */
 /** @jsx jsx */
 import React, { useEffect, useState } from 'react';
 import { css, jsx } from '@emotion/react';
-// import axios from 'axios'; // be used for fetching data from backend
-
 
 export default function Create() {
 
     const [categoriesList, setCategoriesList] = useState([]);
+    const [categoriesListCopy, setCategoriesListCopy] = useState([]);
     const [courses, setCourses] = useState([]);
     const [persons, setPersons] = useState([]);
 
@@ -20,8 +20,9 @@ export default function Create() {
             `http://open-book1.azurewebsites.net/api/course/categories`
         );
         const data = await response.json();
-        console.log(data);
+        console.log("categories: ", data);
         setCategoriesList(data);
+        setCategoriesListCopy(data);
     };
 
     const getCourses = async () => {
@@ -29,7 +30,6 @@ export default function Create() {
             `http://open-book1.azurewebsites.net/api/course`
         );
         const data = await response.json();
-        console.log(data.hits);
         setCourses(data);
         console.log("courses: ", courses);
     };
@@ -39,21 +39,41 @@ export default function Create() {
             `http://open-book1.azurewebsites.net/api/person`
         );
         const data = await response.json();
-        console.log(data.hits);
+        console.log("persons: ", data);
         setPersons(data);
     };
-    console.log("persons: ", persons);
     useEffect(getAuthor, [])
-
     useEffect(getCourses, [])
     useEffect(getCategories, [])
+
+    //search functionality 
+    const handleSearch = async (event) => {
+        let searchQuery = event.target.value.toLowerCase();
+
+        let displayedCat = categoriesListCopy.filter((cat) => {
+            var searchValue = cat.toLowerCase();
+            return searchValue.indexOf(searchQuery) !== -1;
+        });
+
+        if (displayedCat)
+            setCategoriesList(displayedCat)
+        else
+            setCategoriesList(categoriesList)
+    }
 
     return (
         <div >
             <div>
-                <Header title="Browse" />
+                <Header
+                    title="Browse"
+                />
+                <input onChange={handleSearch} type="text" placeholder="Search by category..." />
+                {/* {lessons.number} */}
                 {categoriesList.map(cat => (
-                    <CardSection persons={persons} category={cat} />
+                    <CardSection
+                        persons={persons}
+                        category={cat}
+                    />
                 ))}
             </div>
         </div>
